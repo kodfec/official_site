f23c039f2f663da25fa71d8c9370da9293cd9941
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2023-09-20 12:14:04 +0530

    card 3

--- src/app/component/Card3.tsx
+++ src/app/component/Card3.tsx
@@ -1,30 +1,38 @@
 const Card3 = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-    <div className="text-center mb-6">
-        <span className="text-2xl font-semibold text-blue-600">Our Services</span>
+    <div className="container mx-auto px-4 py-8 mt-60">
+      <div className="text-center mb-6">
+        <span className="text-2xl font-semibold text-blue-600">
+          Our Services
+        </span>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Mobile Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Web Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Web Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Web Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Web Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+          <h5>Web Development</h5>
+          <p>Empowering Your Digital Presence with Expert Web Development</p>
+        </div>
+      </div>
     </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 1</p>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 2</p>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 3</p>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 4</p>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 5</p>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Service 6</p>
-        </div>
-    </div>
-</div>
   );
 };
 
