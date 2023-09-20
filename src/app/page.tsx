a92ea490fcdd675137d613455c9abd045ac1ccfe
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2023-09-20 15:27:08 +0530

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,50 +5,56 @@ import Card2 from "./component/Card2";
 import Card3 from "./component/Card3";
 import Footer from "./component/Footer";
 
-
 export default function Home() {
- 
-
   return (
     <div className="body">
       <NavBar />
 
-      {/* <div> */}
-      {/* first card */}
-      <div className="card bg-slate-400">
-        <div>
-          <Image
-            src={background}
-            alt="background"
-            className="w-full max-h-screen object-cover object-center absolute top-0 left-0"
-          />
+      <div>
+        {/* first card */}
+        <div className="card bg-slate-400">
+          <div>
+            <Image
+              src={background}
+              alt="background"
+              className="w-full max-h-screen object-cover object-center absolute top-0 left-0"
+            />
 
-          <div className=" rounded-md bg-transparent wordcontent lg:top-52">
-            <span className=" font-bold text-lg mb-10 mainspan">
-              Powering Possibilities through Innovative Code
-            </span>
-            <p className=" ml-7 mt-4">
-              Using innovative and cutting-edge coding techniques to create
-              software solutions that open up new possibilities for businesses
-              and individuals.
-            </p>
+            <div className=" rounded-md bg-transparent wordcontent lg:top-52">
+              <span className=" font-bold text-lg mb-10 mainspan">
+                Powering Possibilities through Innovative Code
+              </span>
+              <p className=" ml-7 mt-4">
+                Using innovative and cutting-edge coding techniques to create
+                software solutions that open up new possibilities for businesses
+                and individuals.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* secod card */}
-      <div className="card bg-white">
-        <Card2 />
-      </div>
+        {/* secod card */}
+        <div className="card bg-white">
+          <Card2 />
+        </div>
 
+<<<<<<< Updated upstream
       {/* thrid card */}
       <div className="card bg-blue-500">
         <Card3 />
       </div>
 
       {/* </div> */}
+=======
+        {/* thrid card */}
+        <div className="card bg-blue-500 py-8">
+          <Card3 />
+        </div>
+
+      </div>
+>>>>>>> Stashed changes
       <div className="footer">
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
