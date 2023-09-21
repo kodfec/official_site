d96171ed579f36666fda89a54475e587edff491d
Virul Nirmala <89099391+iamvirul@users.noreply.github.com>
2023-09-22 01:34:35 +0530

    image responsiveness

--- src/app/component/Card2.tsx
+++ src/app/component/Card2.tsx
@@ -5,13 +5,11 @@ const Card2 = () => {
     return (
         <div className="max-w-full w-full min-h-screen flex justify-center items-center ">
             <div className="bg-white p-20 rounded-lg shadow-lg max-w-7xl w-full mx-4 sm:mx-auto">
-               
                     <div className="mb-6 p-2">
                         <span className="animate-spin text-4xl font-semibold text-gray-700 ">WELCOME</span><br />
 
                         <span className=" animate-pulse text-7xl font-semibold text-blue-600">TO KODFEC</span>
                     </div>
-               
                     <p className="text-gray-600 text-xl text-justify w-6/12">
                         Having you here makes us very happy. At KODFEC, we are passionate about
                         developing outstanding software solutions that change the world. To
@@ -19,8 +17,6 @@ const Card2 = () => {
                         team of dedicated experts is committed to offering top-notch services
                         and products.
                     </p>
-             
-    
                 <div className="animate-bounce mt-12 text-center">
                     <span className="text-5xl font-semibold text-green-600">GET IN TOUCH</span>
                 </div>
@@ -29,7 +25,6 @@ const Card2 = () => {
                     us, fill in the form here or please call one of our toll-free numbers
                     below or email using the contact form below.
                 </p>
-
             </div>
         </div>
     );
