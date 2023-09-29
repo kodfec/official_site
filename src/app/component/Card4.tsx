fa00e22b1f8f49895c3b6a030838cc8c7ae689c0
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2023-09-29 11:55:27 +0530

    Landing Page

--- src/app/component/Card4.tsx
+++ src/app/component/Card4.tsx
@@ -1,39 +1,50 @@
 const card4 = () => {
   return (
-    <div className="py-20 ">
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl  ">
+    <div className="py-20 flex flex-col items-center">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">K</p>
           <p className="text-4xl font-bold">- Kinetic</p>
         </div>
-        <p>abcdef</p>
+        <p>abcde</p>
       </div>
 
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl ">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">O</p>
           <p className="text-4xl font-bold">- Oriented</p>
         </div>
-        <p>abcdef</p>
+        <p>
+          "Action is the foundational key of success". Pablo Picasso Kodfec is
+          an action and goal oriented software company which brings your
+          thoughts to reality so that you can be action and goal oriented.
+        </p>
       </div>
 
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl  ">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">D</p>
           <p className="text-4xl font-bold">- Demandable</p>
         </div>
-        <p>abcdef</p>
+        <p>
+          "The universe doesn't give you what you ask for with your thoughts. It
+          gives you what you demand with your actions" We Kodfec helps who put
+          their confidence in us to achieve their objectives.
+        </p>
       </div>
 
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl  ">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">F</p>
           <p className="text-4xl font-bold">- Flexible</p>
         </div>
-        <p>abcdef</p>
+        <p>
+          "Flexibility makes building stronger. Imagine what it can do to your
+          soul" Carlos Barrios We Kodfec is flixible as water.
+        </p>
       </div>
 
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl ">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">E</p>
           <p className="text-4xl font-bold">- Elective</p>
@@ -41,14 +52,16 @@ const card4 = () => {
         <p>abcdef</p>
       </div>
 
-      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl  ">
+      <div className="bg-white shadow-xl mb-8 px-16 py-8 rounded-3xl w-1/3">
         <div className="flex flex-row items-center">
           <p className="text-8xl font-bold">C</p>
           <p className="text-4xl font-bold">- Creative</p>
         </div>
-        <p>abcdef</p>
+        <p>
+          Our comitted and expirienced team will come up with some creative new
+          ways to make your vision a reality
+        </p>
       </div>
-
     </div>
   );
 };
--- src/app/pages/portfolio/page.tsx
+++ src/app/pages/portfolio/page.tsx
@@ -9,72 +9,72 @@ const page = () => {
       <NavBar />
 
       <div className="pt-8 bg-slate-950 flex flex-col items-center lg:grid justify-items-center  grid-cols-2  xl:grid-cols-3">
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2 ">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
           </div>
         </div>
 
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
           </div>
         </div>
 
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
           </div>
         </div>
 
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
           </div>
         </div>
 
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
           </div>
         </div>
 
-        <div className="bg-blue-200 mt-20 w-96 h-96 rounded-3xl flex">
+        <div className="bg-blue-200 mt-20 w-80 h-80 md:w-96 md:h-96 rounded-3xl flex">
           <div className="self-end ps-4 pb-2">
             <p className="font-bold text-2xl">DMS Stores</p>
             <p>POS System</p>
           </div>
-          <div className="self-end ms-36">
+          <div className="self-end ms-24 md:ms-32 lg:ms-36">
             <button>
               <Image src={galleryImage} alt="Gallery" />
             </button>
