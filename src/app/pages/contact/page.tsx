1ce191b5c5efa584d28efd0ca3f4cb1b4adeb21c
Virul Nirmala <89099391+iamvirul@users.noreply.github.com>
2023-09-30 23:16:42 +0530

    changes

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -7,16 +7,24 @@ const page = () => {
       <NavBar />
       <div className="pt-32 w-full bg-white">
         <div className="w-full text-center">
-          <span className="w-full text-5xl font-bold">GET CONNECTED WITH US</span>
+          <span className="w-full text-5xl font-bold">
+            GET CONNECTED WITH US
+          </span>
           <p className="mt-4 text-xl">
             We're here to answer your questions and explore collaboration
             opportunities. Reach out to us anytime – we look forward to
             connecting with you !
           </p>
         </div>
-        <div className="w-full mt-4 pb-6 sm:block md:flex">
-          <div className="sm:w-full md:w-1/2 bg-black h-4"></div>
-          <div className="max-sm:mt-4 sm:mt-4 md:mt-0 sm:w-full md:w-1/2 bg-red-700 h-4"></div>
+        <div className="w-full mt-4 h-screen pb-6 sm:block md:flex">
+          <div className="sm:w-full md:w-1/2 flex flex-column justify-center items-center ">
+            <div className=" h-full w-full bg-red-400">
+              <div className=" h-16 w-10 bg-slate-600">
+j
+              </div>
+            </div>
+          </div>
+          <div className="max-sm:mt-4 sm:mt-4 md:mt-0 sm:w-full md:w-1/2 bg-red-700 "></div>
         </div>
       </div>
       <div className="footer bottom-0 w-full">
